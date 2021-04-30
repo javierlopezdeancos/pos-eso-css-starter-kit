633ebf4a9573def6fb38b339d5da4e7e54fc34c6
Javier López de Ancos <javierlopezdeancos@gmail.com>
2021-04-30 15:21:56 +0200

    chore: add autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
 const postcssPresetEnv = require('postcss-preset-env');
+const autoprefixer =  require('autoprefixer')
 
 module.exports = (ctx) => ({
   map: ctx.options.map,
   parser: ctx.options.parser,
   plugins: [
-    postcssPresetEnv({ stage: 4 })
+    postcssPresetEnv({ stage: 4 }),
+    autoprefixer(),
   ],
 })
