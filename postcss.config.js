6c3a67190963b25c6f1cb70b80697392107a60d4
Javier López de Ancos <javierlopezdeancos@gmail.com>
2021-05-04 14:24:44 +0200

    feat: add extend and import feat

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,16 @@
 const postcssPresetEnv = require('postcss-preset-env');
 const autoprefixer =  require('autoprefixer')
 const cssnano = require('cssnano')
+const atImport = require("postcss-import")
+const simpleExtend = require('postcss-extend');
 
 module.exports = (ctx) => ({
   map: ctx.options.map,
   parser: ctx.options.parser,
   plugins: [
     postcssPresetEnv({ stage: 4 }),
+    atImport({ from: "src/index.css" }),
+    simpleExtend(),
     autoprefixer(),
     cssnano({ preset: 'default' }),
   ],
