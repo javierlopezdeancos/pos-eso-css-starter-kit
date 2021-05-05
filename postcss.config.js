e4029a1445a93b4f7d70081bb87bf4bed7bb0ead
Javier López de Ancos <javierlopezdeancos@gmail.com>
2021-05-05 16:12:44 +0200

    feat: add stylelint linter and format

--- postcss.config.js
+++ postcss.config.js
@@ -3,14 +3,18 @@ const autoprefixer =  require('autoprefixer')
 const cssnano = require('cssnano')
 const atImport = require("postcss-import")
 const simpleExtend = require('postcss-extend');
+const stylelint = require('stylelint');
 
 module.exports = (ctx) => ({
   map: ctx.options.map,
   parser: ctx.options.parser,
   plugins: [
     postcssPresetEnv({ stage: 4 }),
-    atImport({ from: "src/index.css" }),
+    atImport({
+      from: "src/index.css",
+    }),
     simpleExtend(),
+    stylelint(),
     autoprefixer(),
     cssnano({ preset: 'default' }),
   ],
